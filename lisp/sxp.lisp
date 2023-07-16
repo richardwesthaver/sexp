c843e31e6a07632c8bcd3678b05bc3c16610f21f
ellis <ellis@rwest.io>
2023-07-15 23:53:17 -0400

    basic lisp stuff

--- lisp/sxp.lisp
+++ lisp/sxp.lisp
@@ -3,3 +3,22 @@
   (:use :cl))
 
 (in-package :sxp)
+
+(defvar read-table (make-hash-table))
+(defvar write-table (make-hash-table))
+
+(defclass form ()
+  ()
+  (:documentation "loosely typed object representing a SXP form."))
+
+(defclass reader ()
+  ()
+  (:documentation "Reader object which returns raw SXP forms from a stream."))
+
+(defclass writer ()
+  ()
+  (:documentation "Writer object which writes a raw SXP to a stream."))
+
+(defclass fmt ()
+  ()
+  (:documentation "Formatter object which dictates how a SXP form is to be encoded."))
